b36cac149f12dd90d1dbdd103fcc57330dfe6f90
bbadrman <bbadrman@gmail.com>
2021-12-13 23:45:55 +0000

    🎯[for-15] 7. Créer des slugs facilement avec Slugify

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,21 +3,32 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Console\CommandLoader\FactoryCommandLoader;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('FR-fr');
         for ($i = 1; $i <= 30; $i++) {
 
             $ad = new Ad();
-            $ad->setTitle("Titre de l'annonce n°$i")
+
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker->paragraphs(5)). '</p>';
+
+
+
+            $ad->setTitle($title)
            ->setSlug("titre-de-l-annonce-n-$i")
-           ->setCoverImage("http://placeholder.com")
-           ->setIntroduction("Bonjour à tout")
-           ->setContent("<p> Je suis un conent riche !</p>")
+           ->setCoverImage($coverImage)
+           ->setIntroduction($introduction)
+           ->setContent($content)
            ->setPrice(mt_rand(40, 200))
            ->setRooms(mt_rand(1, 5));
             $manager->persist($ad);
