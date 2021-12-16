9d039a385cba12008cc66e74738b7d1ca2b9e21e
bbadrman <bbadrman@gmail.com>
2021-12-16 16:48:41 +0000

    [for-16 ] 3. Comprendre l'Injection de Dépendances

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,11 +13,11 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(): Response
+    public function index(AdRepository $repo): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
+        // $repo = $this->getDoctrine()->getRepository(Ad::class);
         $ads = $repo->findAll();
-        
+
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
         ]);
