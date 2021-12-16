68c529cd696860fe85237b7f7b4b43a37edf2eb2
bbadrman <bbadrman@gmail.com>
2021-12-16 16:34:17 +0000

    [for-15 ] 2. Récupération des annonces grâce au Repository Doctrine

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ad;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
 {
@@ -13,8 +14,11 @@ class AdController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Ad::class);
+        $ads = $repo->findAll();
+        
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads,
         ]);
     }
 }
