7a3b4b7c9c4404b6170ed48a936de514fed7d713
bbadrman <bbadrman@gmail.com>
2021-12-25 22:08:06 +0100

    14. Enregistrer les données du formulaire d'inscription

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
@@ -49,10 +53,26 @@ class AccountController extends AbstractController
      * @return Response 
      */
 
-    public function register(){
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $encoder){
         $user = new User();
 
         $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $hash = $encoder->hashPassword($user, $user->getHash());
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été crée ! Vous pouvez maintenant vous connecter ! "
+            );
+            return $this->redirectToRoute('account_login');
+        }
 
         return $this->render('account/registration.html.twig',[
             'form' => $form->createView()
