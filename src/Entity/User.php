431a603060dcf601531782bcaea114543b60f292
bbadrman <bbadrman@gmail.com>
2021-12-27 20:21:17 +0000

    3. Récupération des rôles dans l'entité User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -250,7 +250,12 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        $roles = $this->userRoles->map(function ($role) {
+            return $role->getTitle();
+        })->toArray();
+
+        $roles[] = 'ROLE_USER';
+        return $roles;
     }
 
     public function getPassword()
