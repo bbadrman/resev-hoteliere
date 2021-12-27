e8bfdc2811d3679a2cf47759b1a509abbb5d2fa6
bbadrman <bbadrman@gmail.com>
2021-12-27 19:56:03 +0000

    1. Mise en place des rôles d'utilisateurs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -85,6 +85,11 @@ class User implements UserInterface
      */
     private $ads;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Role::class, mappedBy="users")
+     */
+    private $userRoles;
+
     public function getFullName(){
         return "{$this->firstName}  {$this->lastName}";
     }
@@ -109,6 +114,7 @@ class User implements UserInterface
     public function __construct()
     {
         $this->ads = new ArrayCollection();
+        $this->userRoles = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -262,5 +268,32 @@ class User implements UserInterface
     
     public function eraseCredentials()
     {
+    }
+
+    /**
+     * @return Collection|Role[]
+     */
+    public function getUserRoles(): Collection
+    {
+        return $this->userRoles;
+    }
+
+    public function addUserRole(Role $userRole): self
+    {
+        if (!$this->userRoles->contains($userRole)) {
+            $this->userRoles[] = $userRole;
+            $userRole->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserRole(Role $userRole): self
+    {
+        if ($this->userRoles->removeElement($userRole)) {
+            $userRole->removeUser($this);
+        }
+
+        return $this;
     }
 }
