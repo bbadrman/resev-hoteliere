40913d90c969ea4eb11e6b442e7df40f68d4ac9f
bbadrman <bbadrman@gmail.com>
2021-12-27 21:47:24 +0000

    7. Permettre aux utilisateurs de supprimer leurs annonces

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -130,4 +130,28 @@ class AdController extends AbstractController
             'ad' => $ad
         ]);
     }
+
+    /**
+     * Permet de supprimer une annonce
+     * 
+     * @Route("/ads{slug}/delete", name="ads_delete")
+     * @Security("is_granted('ROLE_USER') and user == ad.getAuthor()", message="Vous n'avez pas le droit d'accéder a cette ressource")
+     * 
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, EntityManagerInterface $manager)
+    {
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+        );
+
+        return $this->redirectToRoute("ads_index");
+    }
+
 }
