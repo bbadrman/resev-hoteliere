9777b7b5f559ee22232fe85b60ee262d496678c2
bbadrman <bbadrman@gmail.com>
2021-12-28 21:29:00 +0000

    7. Sécuriser la page de réservation avec l'annotation @IsGranted()

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -7,12 +7,14 @@ use App\Entity\Booking;
 use App\Form\BookingType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookingController extends AbstractController
 {
     /**
      * @Route("/ads/{slug}/book", name="booking_create")
+     * @IsGranted("ROLE_USER")
      */
     public function  book(Ad $ad): Response
 
