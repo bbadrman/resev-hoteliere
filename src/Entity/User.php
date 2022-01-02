3006b32b1fbc9bbade825a0ee752b0e9d581e72e
bbadrman <bbadrman@gmail.com>
2022-01-02 18:49:41 +0000

    [for-111 ] 1. Création d'une entité Comment pour stocker les avis des visiteurs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -95,6 +95,11 @@ class User implements UserInterface
      */
     private $bookings;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="author", orphanRemoval=true)
+     */
+    private $comments;
+
     
 
     
@@ -125,6 +130,7 @@ class User implements UserInterface
         $this->ads = new ArrayCollection();
         $this->userRoles = new ArrayCollection();
         $this->bookings = new ArrayCollection();
+        $this->comments = new ArrayCollection();
        
      
     }
@@ -338,6 +344,36 @@ class User implements UserInterface
             // set the owning side to null (unless already changed)
             if ($booking->getBooker() === $this) {
                 $booking->setBooker(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getAuthor() === $this) {
+                $comment->setAuthor(null);
             }
         }
 
