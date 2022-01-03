61d3e4930e0f48f48b1414403c3996ad9ef5f7ce
bbadrman <bbadrman@gmail.com>
2022-01-03 19:25:53 +0100

    [for-120 ] 10. Affichage du formulaire de commentaire sur une réservation

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Entity\Booking;
+use App\Entity\Comment;
 use App\Form\BookingType;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -65,9 +67,13 @@ class BookingController extends AbstractController
      */
     public function show(Booking $booking)
     {
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
 
         return $this->render('booking/show.html.twig', [
-            'booking' => $booking
+            'booking' => $booking,
+              'form'  =>$form->createView()
 
         ]);
     }
