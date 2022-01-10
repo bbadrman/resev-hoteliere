8b41008fc20c5254dd891ddbdaaa998de36ca048
bbadrman <bbadrman@gmail.com>
2022-01-10 20:01:12 +0100

    [for-150 ] 20. Découverte du type de champ EntityType pour l'édition d'une réservation

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,11 +18,44 @@ class AdminBookingController extends AbstractController
      * @Route("/admin/bookings", name="admin_bookings_index")
      * @return Response
      */
-    
+
     public function index( BookingRepository $repo): Response
     {
         return $this->render('admin/booking/index.html.twig', [
             'bookings' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Permet d'éditer une réservation 
+     * 
+     * @Route("/admin/bookings/{id}/edit", name="admin_bookings_edit")
+     */
+
+    public function edit(Booking $booking, Request $request, EntityManagerInterface $manager)
+    {
+
+        $form = $this->createForm(AdminBookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $booking->getAmount(0);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "La réservation n° <strong>{$booking->getId()}</strong> a bien été modifiée !"
+            );
+            return $this->redirectToRoute("admin_bookings_index");
+        }
+
+        return $this->render('admin/booking/edit.html.twig', [
+            'form' => $form->createView(),
+            'booking' => $booking
         ]);
     }
 }
