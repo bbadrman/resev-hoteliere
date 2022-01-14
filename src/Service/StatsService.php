b089fbbf61aa50da47e6898e37b77bcf1ad7945a
bbadrman <bbadrman@gmail.com>
2022-01-14 17:52:09 +0100

    [for-167 ] 4. Extraire la récupération des données du Controller grâce à la notion de Service

---
+++ src/Service/StatsService.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+
+
+class StatsService
+{
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+    public function getStats(){
+        $users    = $this->getUsersCount();
+        $ads      = $this->getAdsCount();
+        $bookings = $this->getBookingsCount();
+        $comments = $this->getCommentsCount(); 
+
+        return compact('users', 'ads', 'bookings', 'comments');
+    }
+    public function getUsersCount(){
+       return  $this->manager->createQuery('SELECT COUNT(u) FROM App\Entity\User u')->getSingleScalarResult();
+      
+    }
+    public function getAdsCount(){
+        return $this->manager->createQuery('SELECT COUNT(a) FROM App\Entity\Ad a')->getSingleScalarResult();
+       
+    }
+    public function getBookingsCount(){
+        return $this->manager->createQuery('SELECT COUNT(b) FROM App\Entity\User b')->getSingleScalarResult();
+       
+    } 
+    public function getCommentsCount(){
+        return $this->manager->createQuery('SELECT COUNT(c) FROM App\Entity\User c')->getSingleScalarResult();
+
+    }
+    public function getAdsStats($direction){
+        return $this->manager->createQuery(
+            'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+            FROM App\Entity\Comment c
+            JOIN c.ad a
+            JOIN a.author u
+            GROUP BY a
+            ORDER BY note ' . $direction
+        )
+        ->setMaxResults(5)
+        ->getResult();
+
+    }
+
+    // public function getBestAds(){
+    //     return $this->manager->createQuery(
+    //         'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+    //         FROM App\Entity\Comment c
+    //         JOIN c.ad a
+    //         JOIN a.author u
+    //         GROUP BY a
+    //         ORDER BY note DESC'
+    //     )
+    //     ->setMaxResults(5)
+    //     ->getResult();
+    // }
+    // public function getWorstAds(){
+    //     return $this->manager->createQuery(
+    //         'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+    //         FROM App\Entity\Comment c
+    //         JOIN c.ad a
+    //         JOIN a.author u
+    //         GROUP BY a
+    //         ORDER BY note ASC'
+    //     )
+    //     ->setMaxResults(5)
+    //     ->getResult();
+    // }
+}
