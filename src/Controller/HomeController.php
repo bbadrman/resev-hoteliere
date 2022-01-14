f63777c1ff1ac8bbf2bc48530b21f7f1f429b823
bbadrman <bbadrman@gmail.com>
2022-01-14 18:31:19 +0100

    [for-168 ] 5. Découverte du QueryBuilder pour remplir la homepage du site

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -12,10 +14,11 @@ class HomeController extends AbstractController
      * @Route("/", name="homepage")
      * 
      */
-    public function index(): Response
+    public function index(AdRepository $adsRespo, UserRepository $userRepo): Response
     {
         return $this->render('home.html.twig', [
-            'controller_name' => 'HomeController',
+            'ads' => $adsRespo->findBestAds(4),
+            'users' => $userRepo->findBestUsers(2)
         ]);
     }
 }
